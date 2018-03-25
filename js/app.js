7999816449b1b2cf334479ba620c706ac9144e01
Anna Zambelli <annazambelli@Annas-MacBook-Pro.local>
2018-03-25 12:21:54 -0500

    mobile site changes
    changed nav from hamburger to top bar

--- js/app.js
+++ js/app.js
@@ -74,22 +74,6 @@
 });
 
 
-// HAMBURGER MENU
-
-  var hamburger = document.querySelector('#hamburger');
-
-  var mNav = document.querySelector('.mobilenav');
-
-  hamburger.onclick = function(){
-
-    var status = mNav.classList.contains('active');
-
-    if(!status) {
-      mNav.classList.add('active');
-    } else {
-      mNav.classList.remove('active');
-    }
-  }
 
 
 
